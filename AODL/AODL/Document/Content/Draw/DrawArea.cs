cea0286f4b17d0e8e54d10b07f04f2003d251af1
larsbm <larsbm>
2007-02-21 20:44:43 +0000

    *** empty log message ***

--- AODL/AODL/Document/Content/Draw/DrawArea.cs
+++ AODL/AODL/Document/Content/Draw/DrawArea.cs
@@ -1,19 +1,40 @@
-/*
- * $Id: DrawArea.cs,v 1.2 2006/02/05 20:02:25 larsbm Exp $
- */
-
-/*
- * License: 
- * GNU Lesser General Public License. You should recieve a
- * copy of this within the library. If not you will find
- * a whole copy at http://www.gnu.org/licenses/lgpl.html .
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: DrawArea.cs,v $
+ *
+ *  $Revision: 1.3 $
+ *
+ *  last change: $Author: larsbm $
  * 
- * Author:
- * Copyright 2006, Kristy Saunders, ksaunders@eduworks.com
- * 
- * Last changes:
- * 01/29/2006 Merged into the new library version. (Lars Behrman)
- */
+ *  Initial Author:
+ *  Copyright 2006, Kristy Saunders, ksaunders@eduworks.com
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
 
 using System;
 using System.IO;
